3b9f567cad0e8d959a0d5efa9e0f652e44a34242
Usama El-Qastaui <usamaelqastaui@gmail.com>
2021-06-05 23:24:30 +0200

    90% logic done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const display = document.querySelector('#display');
 const add = function (num1, num2){
     return num1 + num2;
 };
@@ -13,49 +14,74 @@ const multiply = function (num1, num2){
 const divide = function (num1, num2){
     return num1 / num2;
 };
-
-const operate = function (op, num1 , num2){
-    switch(op){
+let op1 = '';
+let op2 = '';
+let op = '';
+const operate = function (operation , num1 , num2){
+    if(!num2){return num1};
+    switch(operation){
         case '*':
-            return multiply(num1, num2);
+            op1 = multiply(num1, num2);
             break;
         case '/':
-            return divide(num1, num2);
+            op1 = divide(num1, num2);
             break;
         case '+':
-            return add(num1, num2);
+            op1 = add(num1, num2);
             break;
         case '-':
-            return subtract(num1, num2);
+            op1 = subtract(num1, num2);
             break;
         default:
             return "Not a supported operator";
     }
+    op = '';
+    op2 = '';
+    return op1;
 }
 
-let op1 = '';
-let op2 = '';
-let op = '';
+const backspace = function (){
+    if(!op2){
+        op1 = op1.slice(0, op1.length-1);
+        display.textContent = op1;
+    } else {
+        op2 = op2.slice(0, op2.length-1);
+        display.textContent = op2;
+    }
+}
+
 
 const assignOp = function (e, btn){
     switch(btn.value){
+        case 'clear':
+            op1 = '';
+            op2 = '';
+            op = '';
+            display.textContent = '0';
+            break;
+        case 'delete':
+            backspace();
+            break;
         case '*':
         case '/':
         case '+':
         case '-':
             op = btn.value;
+            display.textContent = op;
             break;
         case '=':
-            console.log(operate(op, parseInt(op1), parseInt(op2)));
+            display.textContent = operate(op, parseInt(op1), parseInt(op2));
             break;
         default:
             if(op === ''){
                 op1 = op1.concat(btn.value);
+                display.textContent = op1;
             } else {
                 op2 = op2.concat(btn.value);
+                display.textContent = op2;
             }
-    }
-    
+    };
+    console.log(`op1: ${op1}, op2 ${op2}, op: ${op}`);
 
 
 }
